6afab3fbfde00ccf384968c076b0e0b401adda99
VolodymyrTrykoz <mr.mastachok@gmail.com>
2018-02-11 20:07:21 +0200

    almost done. Prerelease(finished all sections, add responsive design, a little bit of animation and paralax)

--- dist/libs/js/js.js
+++ dist/libs/js/js.js
@@ -1,3 +1,32 @@
-window.onload = function(){
-   
-};
+
+
+window.onload = function () {
+
+	var parallaxBox = document.getElementById ( 'box' );
+	var c1left = document.getElementById ( 'l1' ).offsetLeft,
+	c1top = document.getElementById ( 'l1' ).offsetTop;
+	
+	
+	parallaxBox.onmousemove = function ( event ) {
+		event = event || window.event;
+		var x = event.clientX - parallaxBox.offsetLeft,
+		y = event.clientY - parallaxBox.offsetTop;
+		
+		mouseParallax ( 'l1', c1left, c1top, x, y, 1500 );
+		mouseParallax ( 'l2', c1left, c1top, x, y, 3000 );
+		mouseParallax ( 'l3', c1left, c1top, x, y, 6500 );
+		mouseParallax ( 'l4', c1left, c1top, x, y, 5400 );
+		mouseParallax ( 'l5', c1left, c1top, x, y, 2060 );
+		
+	}
+	
+}
+
+function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
+	var obj = document.getElementById ( id );
+	var parentObj = obj.parentNode,
+	containerWidth = parseInt( parentObj.offsetWidth ),
+	containerHeight = parseInt( parentObj.offsetHeight );
+	obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 2 + left ) ) / containerWidth ) * speed ) +'px';
+	obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
+}
